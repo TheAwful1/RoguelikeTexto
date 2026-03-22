119e16b33c2c80a7953cee8d66612126f1c0884f
Edwin Felipe <105158452+TheAwful1@users.noreply.github.com>
2026-03-22 14:41:35 -0400

    Lo celebrare con vino ambarino mientras termino de escribir esta matanza que hace de mi un asesino.

--- main.c
+++ main.c
@@ -5,15 +5,21 @@
 #include <time.h>
 #include <stdarg.h>
 
-
+//Los arrays con B representan a los NPC amistosos o con los que se puede hablar
 const char *lugares[] = {"Bosque", "Pradera","Jungla","Granja","Laguna Dorada", "Bosque Carmesi"};
-const char *posiciones[] = {"Este", "Norte",  "Sur", "Oeste"};
-const char *tiposBosque[] = {"Hombre Lobo", "Lobo Blanco", "Buho Gigante", "Oso Negro", "Oso Blanco"};
+const char *posiciones[] = {"Oeste", "Norte", "Este", "Sur"};
+const char *tiposBosque[] = { "Lobo Blanco", "Buho Gigante", "Oso Negro", "Oso Blanco"};
+const char *tiposBosqueB[] = {"NPC Bosque Amistoso", "Hombre Lobo"};
 const char *tiposPradera[] = {"Conejo", "Oveja Gigante", "Slime Azul", "Aguila Calva"};
+const char *tiposPraderaB[] = {"NPC Pradera Amistoso"};
 const char *tiposJungla[] = {"Boa Constrictora Gigante", "Rana Venenosa", "Caiman", "Nutria Pantera Negra", "Pirana"};
+const char *tiposJunglaB[] = {""};
 const char *tiposGranja[] = {"Granjero", "Vaca", "Gallina","Oveja", "Gallina Pavo Real"};
+const char *tiposGranjaB[] = {};
 const char *tiposLagDorada[] = {"Mujer Seductora", "Dragon Blanco", "Slime Dorado"};
+const char *tiposLagDoradaB[] = {};
 const char *tiposBCarmesi[] = {"Pulpo Infernal Carmesi", "Zombie Mutante Carmesi", "Slime Carmesi", "Arana Corrupta Carmesi", "Cangrejo Gigante Azul"};
+const char *tiposBCarmesiB[] = {};
 typedef struct 
 {
     const char* tipo;
@@ -23,8 +29,10 @@ typedef struct
 }Entidad;
 typedef struct 
 {
-
-
+    int terreno;
+    int posicion;
+    int cantidadEntidades;
+    const char* entidades[6];
 }Chunk;
 void GuardarChunk(){}
 void GuardarVariosChunk(){}
@@ -38,41 +46,87 @@ int GenerarPosicion( int size){
     return random;  
 }
 const char* GenerarTipo(int TerrenoActual){//Se puede reemplazar con un bucle
+    int AorB = rand() % 2;
     if (TerrenoActual == 0)
     {
-        int tamanotipo = sizeof(tiposBosque)/ sizeof(tiposBosque[0]);
-        int random = rand() % tamanotipo;
-        return tiposBosque[random];  
+        if (AorB = 1)
+        {
+            int tamanotipo = sizeof(tiposBosque)/ sizeof(tiposBosque[0]);
+            int random = rand() % tamanotipo;
+            return tiposBosque[random];  
+        }
+        else{
+            int tamanotipo = sizeof(tiposBosque)/ sizeof(tiposBosque[0]);
+            int random = rand() % tamanotipo;
+            return tiposBosqueB[random];  
+        }      
+       
     }
     else if (TerrenoActual == 1)
-    {
-        int tamaotipo = sizeof(tiposPradera)/ sizeof(tiposPradera[0]);
-        int random = rand() % tamaotipo;
-        return tiposPradera[random];  
+    { if (AorB = 1)
+        {
+            int tamanotipo = sizeof(tiposPradera)/ sizeof(tiposPradera[0]);
+            int random = rand() % tamanotipo;
+            return tiposPradera[random];  
+        }
+        else{
+            int tamanotipo = sizeof(tiposPraderaB)/ sizeof(tiposPraderaB[0]);
+            int random = rand() % tamanotipo;
+            return tiposPraderaB[random];  
+        }
     }
     else if (TerrenoActual == 2)
-    {
-        int tamaotipo = sizeof(tiposJungla)/ sizeof(tiposJungla[0]);
-        int random = rand() % tamaotipo;
-        return tiposJungla[random];  
+    { 
+        if (AorB = 1)
+        {
+            int tamanotipo = sizeof(tiposJungla)/ sizeof(tiposJungla[0]);
+            int random = rand() % tamanotipo;
+            return tiposJungla[random];  
+        }
+        else{
+            int tamanotipo = sizeof(tiposJunglaB)/ sizeof(tiposJunglaB[0]);
+            int random = rand() % tamanotipo;
+            return tiposJunglaB[random];  
+        } 
     }
     else if (TerrenoActual == 3)
-    {
-        int tamaotipo = sizeof(tiposGranja)/ sizeof(tiposGranja[0]);
-        int random = rand() % tamaotipo;
-        return tiposGranja[random];  
+    { if (AorB = 1)
+        {
+            int tamanotipo = sizeof(tiposGranja)/ sizeof(tiposGranja[0]);
+            int random = rand() % tamanotipo;
+            return tiposGranja[random];  
+        }
+        else{
+            int tamanotipo = sizeof(tiposGranjaB)/ sizeof(tiposGranjaB[0]);
+            int random = rand() % tamanotipo;
+            return tiposGranjaB[random];  
+        }
     }
     else if (TerrenoActual == 4)
-    {
-        int tamaotipo = sizeof(tiposLagDorada)/ sizeof(tiposLagDorada[0]);
-        int random = rand() % tamaotipo;
-        return tiposLagDorada[random];  
+    { if (AorB = 1)
+        {
+            int tamanotipo = sizeof(tiposLagDorada)/ sizeof(tiposLagDorada[0]);
+            int random = rand() % tamanotipo;
+            return tiposLagDorada[random];  
+        }
+        else{
+            int tamanotipo = sizeof(tiposLagDoradaB)/ sizeof(tiposLagDoradaB[0]);
+            int random = rand() % tamanotipo;
+            return tiposLagDoradaB[random];  
+        }
     }
     else if (TerrenoActual == 5)
-    {
-        int tamaotipo = sizeof(tiposBCarmesi)/ sizeof(tiposBCarmesi[0]);
-        int random = rand() % tamaotipo;
-        return tiposBCarmesi[random];  
+    { if (AorB = 1)
+        {
+            int tamanotipo = sizeof(tiposBCarmesi)/ sizeof(tiposBCarmesi[0]);
+            int random = rand() % tamanotipo;
+            return tiposBCarmesi[random];  
+        }
+        else{
+            int tamanotipo = sizeof(tiposBCarmesiB)/ sizeof(tiposBCarmesiB[0]);
+            int random = rand() % tamanotipo;
+            return tiposBCarmesiB[random];  
+        }
     }
     else {
         return "El Terreno Actual no existe";
@@ -131,29 +185,36 @@ int main (){
     
     while (true)
     {
+        
         GenerarMenu(posicion);
+        
         int izquierda = (posicion + 3) % 4;
         int derecha = (posicion + 1) % 4;
-        char input = getchar();        
+        char input;
+        scanf("%c", &input);   
         switch (input)
         {
         case '1':
-       
-        GenerarChunkB(posicion);
         
+        GenerarChunkB(posicion);        
         getchar();
+        
             break;
-        case '2':posicion = izquierda;
+        case '2':
+        posicion = izquierda;     
         
-        GenerarChunkB(posicion);
-        
+        GenerarChunkB(posicion);        
         getchar();
-            break;
-        case '3':posicion = derecha;
         
-        getchar();
             break;
-        default:
+        case '3':
+        posicion = derecha;
+        
+        GenerarChunkB(posicion); 
+        getchar();
+        
+            break;
+        default:        
             break;
         }
     }
