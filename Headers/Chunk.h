6abeeb21f3b83ce8e2c1b8339b4c8fdeea51d184
Edwin Felipe <105158452+TheAwful1@users.noreply.github.com>
2026-04-04 13:30:44 -0400

    Ooops.... I think i lost it.

--- Headers/Chunk.h
+++ Headers/Chunk.h
@@ -33,40 +33,6 @@ const char* GenerarBioma(){
     const char* Direccion = posiciones[random];
     return Direccion;    
 }
-int** GenerarPosicion(){//Esto Devuelve una matiz, realmente no se me ocurre que hacer con esto quiza en el futuro lo use para hacer un grid, quiza deba ponerlo a las entidades
-
-} 
-Creatura GenerarCreatura(){
-
-}
-
-const Creatura* GenerarCreaturas(){//Esto Deberia devolver un array de structs de creaturas
- int MaximaCantidadCreatura = 6;
- int min = 1;
- int random = rand() % MaximaCantidadCreatura;
- Creatura creaturas[random]; //En la declaracion el numero de entidades predefinidas es random
- for (int i = 0; i < sizeof(creaturas)/ sizeof(creaturas[0]); i++)
- {
-    creaturas[i] = GenerarCreatura();
- }
- return creaturas;
-
-} 
-
-Objetos GenerarObjeto(){}
-
-const Objetos* GenerarObjetos(){
-    int MaximaCantidadObjetos = 6;
-    int random = rand() % MaximaCantidadObjetos;
-    Objetos objetos[random];
- for (int i = 0; i < random; i++)
- {
-    objetos[i] = GenerarObjeto();
- }
- return objetos;
-
-}//Esto Deberia devolver un array de structs
-
 //En el futuro podria generar un mapa mas grande y complejo usando muchos chunks
 Chunk GenerarChunk(){
     Chunk chunk;
--- Headers/Entidades.h
+++ Headers/Entidades.h
@@ -1,3 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <time.h>
+#include <stdarg.h>
+#include <conio.h>
+
 const char *tiposBosque[] = { "Lobo Blanco", "Buho Gigante", "Oso Negro", "Oso Blanco"};
 const char *tiposBosqueB[] = {"NPC Bosque Amistoso", "Hombre Lobo"};
 const char *tiposPradera[] = {"Conejo", "Oveja Gigante", "Slime Azul", "Aguila Calva"};
@@ -5,7 +13,7 @@ const char *tiposPraderaB[] = {"NPC Pradera Amistoso"};
 const char *tiposJungla[] = {"Boa Constrictora Gigante", "Rana Venenosa", "Caiman", "Nutria Pantera Negra", "Pirana"};
 const char *tiposJunglaB[] = {""};
 const char *tiposGranja[] = { "Vaca", "Gallina","Oveja", "Gallina Pavo Real"};
-const char *tiposGranjaB[] = {"Granjero","NPC Grajero amistoso"};
+const char *tiposGranjaB[] = {"Granjero","NPC Granjero amistoso"};
 const char *tiposLagDorada[] = { "Dragon Blanco", "Slime Dorado"};
 const char *tiposLagDoradaB[] = {"Mujer Seductora"};
 const char *tiposBCarmesi[] = {"Pulpo Infernal Carmesi", "Zombie Mutante Carmesi", "Slime Carmesi", "Arana Corrupta Carmesi", "Cangrejo Gigante Azul"};
@@ -22,16 +30,24 @@ typedef struct Entidades
     int FragmentosAlma;
     int CapacidadActual;
     int CapacidadPosible;
+    Tipo Tipo;
 };
 
 typedef struct Creatura //Como se le añadira comportamiento?
 {
-    Entidades NPC;
+    
+    int Fuerza;
+    int Velocidad;
+    int Resistencia;
+    Tipo TipoCreatura;
     bool Vivo;
 };
 
 typedef struct Jugador
-{
+{   
+    int Fuerza;
+    int Velocidad;
+    int Resistencia;
     Entidades P1;
     bool Vivo;
     Inventario inventario;
@@ -39,20 +55,66 @@ typedef struct Jugador
 
 typedef struct Inventario
 {
-    const Objetos* Inventario;
+    const Objeto* Inventario; //Array de Objetos
 };
 
-typedef struct Objetos
+typedef struct Objeto
 {
     Entidades atributos;
     bool Roto;
-    Tipo TipoObjeto;
-
+    bool TipoObjeto; 
 };
+typedef struct NPC
+{
+    Tipo TipoNPC;
+};
+
 typedef struct Tipo
 {
-    int TipoObjeto;
+   bool Objeto;
+   bool Creatura;
+   bool NPC;
 };
+
+typedef struct TipoObjeto
+{
+};
+
+
+int** GenerarPosicion(){//Esto Devuelve una matiz, realmente no se me ocurre que hacer con esto quiza en el futuro lo use para hacer un grid, quiza deba ponerlo a las entidades
+
+} 
+
+Creatura GenerarCreatura(){
+
+}
+
+const Creatura* GenerarCreaturas(){//Esto Deberia devolver un array de structs de creaturas
+ int MaximaCantidadCreatura = 6;
+ int min = 1;
+ int random = rand() % MaximaCantidadCreatura;
+ Creatura creaturas[random]; //En la declaracion el numero de entidades predefinidas es random
+ for (int i = 0; i < sizeof(creaturas)/ sizeof(creaturas[0]); i++)
+ {
+    creaturas[i] = GenerarCreatura();
+ }
+ return creaturas;
+
+} 
+
+Objetos GenerarObjeto(){}
+
+const Objetos* GenerarObjetos(){
+    int MaximaCantidadObjetos = 6;
+    int random = rand() % MaximaCantidadObjetos;
+    Objetos objetos[random];
+ for (int i = 0; i < random; i++)
+ {
+    objetos[i] = GenerarObjeto();
+ }
+ return objetos;
+
+}//Esto Deberia devolver un array de structs
 
 
 
